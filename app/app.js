4c2a597101387f019a873fe76b49f93e08c48abd
Vasiliy Vanchuk <v.vanchuk@tut.by>
2018-10-30 17:26:57 +0400

    Update app configuration

--- app/app.js
+++ app/app.js
@@ -2,9 +2,10 @@ import Application from '@ember/application';
 import Resolver from './resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
+import widgetConfig from './widgetConfig';
 
 const App = Application.extend({
-  rootElement: '#simple-practice-widget',
+  rootElement: widgetConfig.el,
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
   Resolver,
